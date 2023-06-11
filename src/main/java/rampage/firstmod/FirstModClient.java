6ce70ea37518603a4f578ba5bef49eb212d61d59
BuildTools <unconfigured@null.spigotmc.org>
2023-06-10 22:00:16 -0400

    Initial commit

---
+++ src/main/java/rampage/firstmod/FirstModClient.java
@@ -0,0 +1,10 @@
+package rampage.firstmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class FirstModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
