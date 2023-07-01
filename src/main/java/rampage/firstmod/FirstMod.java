021c4976351eca678532fc655a27e0969373febb
spinalrampage <spinalrampage@protonmail.com>
2023-06-30 22:13:51 -0400

    Gamer Debris Now Spawns in the Overworld from y 20 to y 5

--- src/main/java/rampage/firstmod/FirstMod.java
+++ src/main/java/rampage/firstmod/FirstMod.java
@@ -7,6 +7,8 @@ package rampage.firstmod;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.fabricmc.fabric.api.biome.v1.BiomeModifications;
+import net.fabricmc.fabric.api.biome.v1.BiomeSelectors;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.fabricmc.fabric.api.registry.FuelRegistry;
@@ -14,7 +16,11 @@ import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroups;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
 import net.minecraft.util.Identifier;
+import net.minecraft.world.gen.GenerationStep;
+import net.minecraft.world.gen.feature.PlacedFeature;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -28,6 +34,9 @@ public class FirstMod implements ModInitializer {
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
     public static final Logger LOGGER = LoggerFactory.getLogger(FirstMod.MOD_ID);
+
+	// Gamer Debris PlacedFeature RegistryKey
+	public static final RegistryKey<PlacedFeature> CUSTOM_ORE_PLACED_KEY = RegistryKey.of(RegistryKeys.PLACED_FEATURE, new Identifier("rampages-gamer-tools","gamer_debris"));
 
 	@Override
 	public void onInitialize() {
@@ -58,6 +67,8 @@ public class FirstMod implements ModInitializer {
 
 		//ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(content -> { content.add(Blocks.GamerDebris); });
 
+		// Gamer Debris Ore Spawning
+		BiomeModifications.addFeature(BiomeSelectors.foundInOverworld(), GenerationStep.Feature.UNDERGROUND_ORES, CUSTOM_ORE_PLACED_KEY);
 
 		// Logger Stuff? Idk
 		LOGGER.info("Hello Fabric world!");
